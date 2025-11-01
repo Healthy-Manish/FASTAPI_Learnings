03ac7b326fb87c361f6696d2d67c19a3b067c17e
healthy_manish <manishdwivedi45165@gmail.com>
2025-11-01 22:07:10 +0530

    included gitignore and json file

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+import json
 app = FastAPI()
 
+
+def load_data():
+    with open("patients.json",'r') as f:
+        data = json.load(f)
+    return data
 @app.get("/")
 def hello():
     return {"message": "hello world"}
@@ -9,3 +14,8 @@ def hello():
 @app.get("/about")
 def about():
     return {"message":"This is my about section"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
